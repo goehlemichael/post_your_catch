381e97e33d0cd1b25309c45c505fa2bbfeffea13
mike <goehlemichael@hotmailcom>
2017-05-30 11:54:03 -0400

    created basic crud for catches

--- main.py
+++ main.py
@@ -1,54 +1,48 @@
-from flask import Flask, render_template, redirect, request
+from flask import Flask, request, flash, url_for, redirect, render_template
 from flask_sqlalchemy import SQLAlchemy
 import os
 
-## Define App
 app = Flask(__name__)
-
-## Define Base Directory
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.sqlite')
+app.config['SECRET_KEY'] = "random string"
+
 db = SQLAlchemy(app)
 
-## Define Model
-class Note(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(80))
-    body = db.Column(db.Text)
+class catches(db.Model):
+   id = db.Column('student_id', db.Integer, primary_key = True)
+   name = db.Column(db.String(100))
+   fish = db.Column(db.String(50))
+   amount = db.Column(db.String(200)) 
+   contact = db.Column(db.String(10))
 
-    def __init__(self, title, body):
-        self.title = title
-        self.body = body
+def __init__(self, name, fish, amount, contact):
+   self.name = name
+   self.fish = fish
+   self.amount = amount
+   self.contact = contact
 
-## routes + controllers
+@app.route('/')
+def show_all():
+   return render_template('show_all.html', catches = catches.query.all() )
 
-## home route
-@app.route("/")
-def home():
-    return render_template("home.html")
+@app.route('/new', methods = ['GET', 'POST'])
+def new():
+   if request.method == 'POST':
+      if not request.form['name'] or not request.form['fish'] or not request.form['contact']:
+         flash('Please enter all the fields', 'error')
+      else:
+         name = request.form['name']
+         fish = request.form['fish']
+         amount = request.form['amount']
+         contact = request.form['contact']
+         catch = catches(name=name, fish=fish, amount=amount, contact=contact)
+         db.session.add(catch)
+         db.session.commit()
+         flash('Record was successfully added')
+         return redirect(url_for('show_all'))
+   return render_template('new.html')
 
-## create post route
-@app.route("/notes/create", methods=["GET", "POST"])
-
-def create_note():
-    if request.method == "GET":
-        return render_template("create_note.html")
-    else:
-        title = request.form["title"]
-        body = request.form["body"]
-        note = Note(title=title, body=body)
-        db.session.add(note)
-        db.session.commit()
-        return redirect("/notes/create")
-
-## get posts route
-@app.route("/notes", methods=["GET"])
-
-def get_notes():
-    return render_template("notes.html", notes = Note.query.all() )
-
-    ## return render_template('notes.html', notes=query.all())
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+   db.create_all()
+   app.run(debug = True)
